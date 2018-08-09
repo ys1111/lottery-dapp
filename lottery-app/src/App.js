675fbe6680f53a3e7b12b9ee5e0b3157c31f67df
ys1111 <y0suke111152@gmail.com>
2018-08-10 02:29:50 +0900

    add materialui

--- lottery-app/src/App.js
+++ lottery-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Paper, AppBar, Typography, withStyles, Toolbar, Divider, CircularProgress, Button } from '@material-ui/core';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery';
@@ -9,7 +10,8 @@ class App extends Component {
     players: [],
     balance: '',
     value: '',
-    message: ''
+    message: '',
+    isLoading: false
   };
 
   async componentDidMount() {
@@ -25,64 +27,97 @@ class App extends Component {
 
     const accounts = await web3.eth.getAccounts();
 
-    this.setState({ message: 'トランザクションを待ってます...' });
+    this.setState({ message: 'トランザクションを待ってます...', isLoading: true });
 
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei(this.state.value, 'ether')
     });
 
-    this.setState({ message: '抽選に参加しました!' });
+    this.setState({ message: '抽選に参加しました!', isLoading: false });
   };
 
   onClick = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    this.setState({ message: 'トランザクションを待ってます...' });
+    this.setState({ message: 'トランザクションを待ってます...', isLoading: true });
 
     await lottery.methods.pickWinner().send({
       from: accounts[0]
     });
 
-    this.setState({ message: '抽選が選ばれました!' });
+    this.setState({ message: '抽選が選ばれました!', isLoading: false });
   };
 
+  
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
-        <h2>抽選ゲーム Contract</h2>
-        <p>
-          このコントラクトは {this.state.manager}　さんによって運営されてます。
-          現在{' '}
-          {this.state.players.length} 人が参加中です、 現在{' '}
-          {web3.utils.fromWei(this.state.balance, 'ether')} ETHがプールされています!
-        </p>
+        <AppBar position="static" className={classes.height} >
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              抽選 dAppsゲーム
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-        <hr />
+        <Paper className={classes.root} elevation={1}>
+          <p>
+            このコントラクトは {this.state.manager}　さんによって運営されてます。
+            現在{' '}
+            {this.state.players.length} 人が参加中です、 現在{' '}
+            {web3.utils.fromWei(this.state.balance, 'ether')} ETHがプールされています!
+          </p>
 
-        <form onSubmit={this.onSubmit}>
-          <h4>抽選エントリー</h4>
-          <div>
-            <label>いくら送金しますか？</label>
-            <input
-              value={this.state.value}
-              onChange={event => this.setState({ value: event.target.value })}
-            />
-          </div>
-          <button>送金</button>
-        </form>
+          <Divider />
 
-        <hr />
+          <form onSubmit={this.onSubmit}>
+            <h4>抽選エントリー</h4>
+            <div>
+              <label>いくら送金しますか？</label>
+              <input
+                value={this.state.value}
+                onChange={event => this.setState({ value: event.target.value })}
+              />
+            </div>
+            <Button>送金</Button>
+          </form>
 
-        <h4>抽選結果</h4>
-        <button onClick={this.onClick}>抽選する</button>
+          <Divider />
 
-        <hr />
+          <h4>抽選結果</h4>
+          <Button onClick={this.onClick}>抽選する</Button>
 
-        <h1>{this.state.message}</h1>
+          <Divider />
+
+          {this.state.isLoading
+          ? <div>
+              <h1>{this.state.message}</h1>
+              <CircularProgress className={classes.progress} size={50} />
+            </div>
+          : <h1>{this.state.message}</h1>}
+        </Paper>
       </div>
     );
   }
 }
 
-export default App;
+const styles = theme => ({
+      root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alineItems: 'center',
+        flexGrow: 1,
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
+        margin: '40px'
+      },
+      appbar: {
+        height: '80'
+      }
+})
+
+export default withStyles(styles)(App);
--- lottery-app/src/index.js
+++ lottery-app/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MaterialApp = (props) => {
+  return(
+    <React.Fragment>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </React.Fragment>
+  )
+}
+
+ReactDOM.render(<MaterialApp />, document.getElementById('root'));
 registerServiceWorker();
